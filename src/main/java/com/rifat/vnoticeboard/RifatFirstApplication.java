d4470c21f30c0eeea0ed857bc35484ba62e81db6
sahariardev <rifatsahariar>
2017-07-03 22:39:13 +0600

    added all

---
+++ src/main/java/com/rifat/vnoticeboard/RifatFirstApplication.java
@@ -0,0 +1,12 @@
+package com.rifat.vnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifatFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifatFirstApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rifat/vnoticeboard/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.rifat.vnoticeboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller 
+public class HomeController {
+
+	@RequestMapping(value="/" , method = RequestMethod.GET) 
+	public String showhome()
+	{
+		return "home";
+	}
+}
